2f0892aeeae45f7f2bc03152eb22a7e6aeffad7b
Matteo Senese <matteo.senese@gmail.com>
2018-11-09 18:07:50 +0100

    Deserialization inserted
    - Detached thread
    - Deserialization
    - Data receiver method updated

--- wifi_watchdog_analyzer/Receiver.cpp
+++ wifi_watchdog_analyzer/Receiver.cpp
@@ -13,90 +13,16 @@ Receiver::~Receiver()
 /* Define a data id to make possible a resend in case of error (??)*/
 PACKET_T Receiver::operator() ()
 {
-	string hash_buff(HASH_LENGHT, 0),
-					  ssid_buff(SSID_LENGHT, 0);
-	int rssi;
-	UINT32 seq_number;
-	UINT8 channel,
-			ssid_len;
-	mac_t macaddr;
-	UINT16 off_time;
-	SSIZE_T byte;
-
+	char* recv_buff;
+	SSIZE_T bytes;
 	string fail = string("recv() failed");
-	
-	// channel
-	byte = recv(this->sock, (char*)&channel, sizeof(UINT8), 0);
-	if (byte == 0) //connection closed by peer
+	bytes = recv(this->sock, recv_buff, sizeof(PACKET_T), 0);
+	if (bytes == 0) //connection closed by peer
 		throw Recv_exception(fail.append(": connection closed by peer"));
-	else if (byte < 0) //invalid socket or connection closed
+	else if (bytes < 0) //invalid socket or connection closed
 		throw Sock_exception(fail);
 
-	// hash
-	byte = recv(this->sock, &hash_buff[0], HASH_LENGHT, 0);
-	if (byte == 0)
-		throw Recv_exception(fail.append(": connection closed by peer"));
-	else if (byte < 0)
-		throw Sock_exception(fail);
-
-	// mac
-	for (int i = 0; i < MAC_BYTES; i++)
-	{
-		byte = recv(this->sock, (char*)&(macaddr.raw_mac[i]), sizeof(unsigned char), 0);
-		if (byte == 0)
-			throw Recv_exception(fail.append(": connection closed by peer"));
-		else if (byte < 0)
-			throw Sock_exception(fail);
-	}	
-
-	// rssi
-	byte = recv(this->sock, (char*)&rssi, sizeof(int), 0);
-	if (byte == 0)
-		throw Recv_exception(fail.append(": connection closed by peer"));
-	else if (byte < 0)
-		throw Sock_exception(fail);
-
-	// sequence number
-	byte = recv(this->sock, (char*)&seq_number, sizeof(UINT32), 0);
-	if (byte == 0)
-		throw Recv_exception(fail.append(": connection closed by peer"));
-	else if (byte < 0)
-		throw Sock_exception(fail);
-
-	//ssid len
-	byte = recv(this->sock, (char*)&ssid_len, sizeof(UINT8), 0);
-	if (byte == 0)
-		throw Recv_exception(fail.append(": connection closed by peer"));
-	else if (byte < 0)
-		throw Sock_exception(fail);
-	
-	// ssid
-	if (ssid_len > 0)
-	{
-		byte = recv(this->sock, &ssid_buff[0], ssid_len, 0);
-		if (byte == 0)
-			throw Recv_exception(fail.append(": connection closed by peer"));
-		else if (byte < 0)
-			throw Sock_exception(fail);
-	}
-	ssid_buff.resize(ssid_len);
-
-	// raw timestamp
-	byte = recv(this->sock, (char*)&off_time, sizeof(UINT16), 0);
-	if (byte == 0)
-		throw Recv_exception(fail.append(": connection closed by peer"));
-	else if (byte < 0)
-		throw Sock_exception(fail);
-
-	PACKET_T pack;
-	pack.channel = channel;
-	pack.hash = string(hash_buff);
-	pack.ssid = string(ssid_buff);
-	pack.rssi = ntohl(rssi);
-	pack.mac_addr = macaddr;
-	pack.off_timestamp = ntohs(off_time); //receiver can sets the offset only
-	pack.seq_number = ntohs(seq_number);
-
+	PACKET_T pack = deserialize(recv_buff);
 	return pack;
 }
 
--- wifi_watchdog_analyzer/packet.h
+++ wifi_watchdog_analyzer/packet.h
@@ -10,26 +10,26 @@
 #include <string>
 #include <sstream>
 
-#define MAC_BYTES 6
-#define HASH_LENGHT 32 
-#define SSID_LENGHT 32
+#define MAC_LENGTH 6
+#define MD5_HASH_LENGTH 32
+#define MAX_SSID_LENGTH 32
 
 union mac_t
 {
-	unsigned char raw_mac[MAC_BYTES];
+	unsigned char raw_mac[MAC_LENGTH];
 	UINT64 compacted_mac : 48;
 };
 
 typedef struct
 {
-	std::string hash;
-	std::string ssid;
+	int channel;
 	int rssi;
+	int sequence_ctrl;
+	int timestamp;
+	int ssid_length;
 	mac_t mac_addr;
-	UINT8 channel;
-	UINT16 off_timestamp; //offset in seconds given by the boards
-	time_t timestamp; //in unix time
-	UINT32 seq_number;
+	string ssid;
+	string hash;
 } PACKET_T;
 
 
@@ -37,12 +37,12 @@ typedef struct
 inline std::string mactos(mac_t macaddr)
 {
 	std::stringstream ss;
-	for (int i = 0; i < MAC_BYTES; i++)
+	for (int i = 0; i < MAC_LENGTH; i++)
 	{
 		if ( !(macaddr.raw_mac[i] & 0xF0) ) //if the leading digit of a couple is 0 ==> insert it manually
 			ss << '0';
 		ss << std::hex << (size_t)macaddr.raw_mac[i];
-		if(i < MAC_BYTES - 1)
+		if(i < MAC_LENGTH - 1)
 			ss << ':';
 	}
 	return ss.str();
@@ -77,3 +77,67 @@ inline void UnixTimeToSystemTime(time_t t, LPSYSTEMTIME pst)
 }
 
 
+PACKET_T inline deserialize(char *buf) {
+	int channel;
+	int rssi;
+	int sequence_ctrl;
+	int timestamp;
+	int ssid_length;
+	char mac_addr[MAC_LENGTH];
+	char ssid[MAX_SSID_LENGTH + 1];
+	char hash[MD5_HASH_LENGTH + 1];
+
+	int *buf_int = (int *)buf;
+	channel = ntohl(*buf_int); //CHANNEL
+	buf_int++;
+	rssi = ntohl(*buf_int); //RSSI
+	buf_int++;
+	sequence_ctrl = ntohl(*buf_int); //SEQUENCE CTRL
+	buf_int++;
+	timestamp = ntohl(*buf_int); //TIMESTAMP
+	buf_int++;
+	ssid_length = ntohl(*buf_int); //SSID LENGTH
+	buf_int++;
+
+	char *buf_c = (char *)buf_int;
+
+	for (int i = 0; i < MAC_LENGTH; i++) {
+		mac_addr[i] = *buf_c;
+		buf_c++;
+	}
+
+	for (int i = 0; i < MD5_HASH_LENGTH; i++) {
+		hash[i] = *buf_c;
+		buf_c++;
+	}
+	hash[MD5_HASH_LENGTH] = '\0';
+
+	for (int i = 0; i < ssid_length; i++) {
+		ssid[i] = *buf_c;
+		buf_c++;
+	}
+	ssid[ssid_length] = '\0';
+
+	printf("CH=%d, RSSI=%02d, MAC=%02x:%02x:%02x:%02x:%02x:%02x, SSID=%s, SEQ=%d, TIMESTAMP: %d, HASH=%s\n",
+		channel,
+		rssi,
+		(uint8_t)mac_addr[0], (uint8_t)mac_addr[1], (uint8_t)mac_addr[2],
+		(uint8_t)mac_addr[3], (uint8_t)mac_addr[4], (uint8_t)mac_addr[5],
+		ssid,
+		sequence_ctrl,
+		timestamp,
+		hash
+	);
+
+	PACKET_T pack;
+	pack.channel = channel;
+	pack.rssi = rssi;
+	for(int i = 0; i < MAC_LENGTH; i ++)
+		pack.mac_addr.raw_mac[i] = mac_addr[i];
+	pack.ssid = std::string(ssid);
+	pack.sequence_ctrl = sequence_ctrl;
+	pack.timestamp = timestamp;
+	pack.hash = hash;
+
+
+}
