d605c270170fdd7f2d328e7c93bd4902f20626d9
Daniel Rusu <rusudaniel39@gmail.com>
2019-04-03 21:12:01 +0100

    Added exception type in db_exception

--- src/db_exception.h
+++ src/db_exception.h
@@ -1,24 +1,43 @@
 #pragma once
 
 #include <string>
-using namespace std;
 
-class db_exception : public std::exception
-{
-public:
+namespace db {
     
-    db_exception() : errmsg("DB_ERROR: unknown") {}
+    
 
-    db_exception(const char* msg) {
-        this->errmsg = "DB_ERROR: " + string(msg);
-    }
-
-    virtual const char* what() const throw()
+    class db_exception : public std::exception
     {
-        return (this->errmsg.c_str());
-    }
+    public:
+
+        enum type {
+            error,              // unrecovable error
+            constraint_error    // trying to insert a duplicate tuple
+        };
     
-private:
-    string errmsg;
-};
+        db_exception() : 
+            errmsg("DB_ERROR: unknown"), 
+            errtype(error) {}
+
+        db_exception(const char* msg, type etype = error) {
+            this->errmsg = "DB_ERROR: " + std::string(msg);
+            errtype      = etype;
+        }
+
+        virtual const char* what() const throw()
+        {
+            return (this->errmsg.c_str());
+        }
+
+        virtual const type why() 
+        { 
+            return this->errtype; 
+        }
+    
+    private:
+        std::string errmsg;
+        type        errtype;
+    };
+}
+
 
