2ce8b98211267223216ab7a65959a186418a9164
Daniel Rusu <rusudaniel39@gmail.com>
2019-04-27 10:26:04 +0100

    added function in packet_shunter for packet processing and localization

--- src/utils/include/packet_shunter.h
+++ src/utils/include/packet_shunter.h
@@ -2,33 +2,47 @@
 #define PACKET_SHUNTER_H_INCLUDED
 #pragma once
 
+#include "cfg.h"
 #include "packet.h"
+#include "device.h"
 #include <string>
 #include <cstdint>
 #include <map>
 #include <mutex>
 
+constexpr long int default_max_container_size = 1000;
 
 class packet_shunter {
 
 public:
-    packet_shunter(int in_anchors_number);
+    packet_shunter(int anchors_nr);
     ~packet_shunter();
 
     void submit_packet(packet new_packet);
 
 private:
+    device process_readings(
+        packet new_packet, 
+        std::map<uint64_t, int32_t> readings);
+
+    void store_data(
+        packet new_packet, 
+        device new_device);
+
+
+    void clean_container();
+
 
     int anchors_number = 0;
 
     /* [packet hash -> packet] */
-    std::map<std::string, packet> packets;
+//    std::map<std::string, packet> packets;
     /* [packet hash -> [anchor mac -> rssi measurement] */
     std::map<std::string, std::map<uint64_t, int32_t>> rssi_readings;
 
     std::mutex packets_lock;
 
-
+    std::shared_ptr<cfg::configuration> context;
 };
 
 #endif // !PACKET_SHUNTER_H_INCLUDED
--- src/utils/src/packet_shunter.cpp
+++ src/utils/src/packet_shunter.cpp
@@ -1,7 +1,9 @@
 #include "packet_shunter.h"
+#include "localization.h"
+#include <cassert>
 
-packet_shunter::packet_shunter(int in_anchors_number) :
-    anchors_number(in_anchors_number)
+packet_shunter::packet_shunter(int anchors_nr) :
+    anchors_number(anchors_nr)
 {
 
 }
@@ -16,10 +18,10 @@ packet_shunter::submit_packet(packet new_packet)
     uint64_t anchor_mac = new_packet.anchor_mac;
     
     
-    auto find_hash = rssi_readings.find(hash);
+    //auto find_hash = rssi_readings.find(hash);
 
-    if (find_hash == rssi_readings.end())
-        packets[hash] = new_packet;
+    //if (find_hash == rssi_readings.end())
+    //    packets[hash] = new_packet;
 
     rssi_readings[hash][anchor_mac] = rssi;
 
@@ -29,5 +31,61 @@ packet_shunter::submit_packet(packet new_packet)
         // localize
         // insert in db
         // return
+        
+        auto readings = rssi_readings.extract(hash);
+        
+        guard.release();
+        device new_device = process_readings(new_packet, readings.mapped());
+        store_data(new_packet, new_device);
     }
+
+    if (guard.owns_lock() == false)
+        guard.lock();
+
+    if (rssi_readings.size() > default_max_container_size)
+        clean_container();
+}
+
+device
+packet_shunter::process_readings(
+    packet new_packet,
+    std::map<uint64_t, int32_t> readings)
+{
+    std::vector<std::pair<Point2d, int>> measurements;
+
+    for (auto reading : readings) {
+        std::pair<double, double> anchor_coordinates;
+        bool valid_position = 
+            context->get_anchor_position(reading.first, anchor_coordinates);
+        
+        //if (valid_position == false)
+        // TODO: check errors
+        
+        Point2d anchor_point(anchor_coordinates.first, anchor_coordinates.second);
+
+        measurements.push_back(std::make_pair(anchor_point, reading.second));
+    }
+
+    assert(measurements.size() == anchors_number);
+
+    Point2d device_position = weighted_loc(measurements);
+
+    device rval(
+        new_packet.device_mac, new_packet.timestamp, 
+        device_position.m_x(), device_position.m_y());
+
+    return rval;
+}
+
+void
+packet_shunter::store_data(
+    packet new_packet,
+    device new_device)
+{
+}
+
+void
+packet_shunter::clean_container()
+{
+
 }
